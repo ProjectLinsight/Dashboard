a092e793ad1e44ce92f0b973e2b4a8a32444b7f0
dilky97 <2017cs039@stu.ucsc.cmb.ac.lk>
2020-06-30 01:52:59 +0530

    bugs in xapi statements retrieval

--- app/Http/Controllers/Shared/sharedXapi.php
+++ app/Http/Controllers/Shared/sharedXapi.php
@@ -9,9 +9,10 @@ use Illuminate\Support\Facades\DB;
 class sharedXapi extends Controller{
     public function getData(){
         $statements = DB::connection('mysql2')->table('trax_xapiserver_statements')->get();
+        $stmt_count = count($statements);
         
         $state = array();
-        for($i=0;$i<20;$i++){
+        for($i=0;$i<$stmt_count;$i++){
             $temp = json_decode($statements[$i]->data);
             $logArray=explode("/",$temp->verb->id);
             if($logArray[sizeof($logArray)-1]==="visited"){
@@ -21,9 +22,9 @@ class sharedXapi extends Controller{
             }
             $state[$i]['user'] = $temp->actor->account->name ;
             $state[$i]['title'] = $temp->object->definition->name->en ;
-            $state[$i]['description'] = $temp->object->definition->description->en ;
+            $state[$i]['definition'] = $temp->object->definition ;
             $state[$i]['timestamp'] = $temp->timestamp ;
         }
-        return $state;
+        dd($state) ;
     }
 }
