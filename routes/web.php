234edfb1039fdaedc43f08b2f58b212a5646a1c0
sathananthan-malan97 <sathananthanmalan7@gmail.com>
2020-07-09 00:00:55 +0530

    Update user name

--- routes/web.php
+++ routes/web.php
@@ -41,7 +41,8 @@ Route::post('/post','Student\PostsController@store')->middleware('student');
 Route::get('/post/{post}/{user_id}/edit','Student\PostsController@edit')->name('post.edit')->middleware('student');
 Route::patch('/post/{post}/{user_id}/update','Student\PostsController@update')->name('post.update')->middleware('student');
 Route::get('/user/{user_id}/edit','Student\ProfilesController@edit')->name('profile.edit')->middleware('student');
-Route::patch('/user/{user_id}/update','Student\ProfilesController@update')->name('profile.update')->middleware('student');
+Route::post('/user/update_name/{user_id}','Student\ProfilesController@update')->middleware('student');
+Route::post('/user/update_photo/{user_id}','Student\ProfilesController@update1')->middleware('student');
 
 //Lecturer
 Route::get('/lecturer/lecturer_home', 'Lecturer\HomeController@index')->middleware('lecturer');
