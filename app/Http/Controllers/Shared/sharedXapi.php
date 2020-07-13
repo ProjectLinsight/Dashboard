314b6078e98e26868638e71a97f4d89256dfc111
dilky97 <2017cs039@stu.ucsc.cmb.ac.lk>
2020-07-13 21:45:01 +0530

    xapi retrieval changed for logstore compatibility

--- app/Http/Controllers/Shared/sharedXapi.php
+++ app/Http/Controllers/Shared/sharedXapi.php
@@ -9,24 +9,27 @@ use Illuminate\Support\Facades\DB;
 class sharedXapi extends Controller{
     public function getData(){
         $statements = DB::connection('mysql2')->table('trax_xapiserver_statements')->get();
+        //$stmt_json = json_encode($statements);
         $stmt_count = count($statements);
         
-        $state = array();
+        $stmt_arr = array();
         for($i=0;$i<$stmt_count;$i++){
             $temp = json_decode($statements[$i]->data);
-            $logArray=explode("/",$temp->verb->id);
-            if($logArray[sizeof($logArray)-1]==="visited"){
-                $state[$i]['location'] = "outside" ;
-                $state[$i]['verb'] = $logArray[sizeof($logArray)-1];
-            }else{
-                $state[$i]['location'] = "inside" ;
-                $state[$i]['verb'] = $logArray[sizeof($logArray)-1];
-            }
-            $state[$i]['user'] = $temp->actor->account->name ;
-            $state[$i]['title'] = $temp->object->definition->name->en ;
+            array_push($stmt_arr,$temp);
+            
+            // $logArray=explode("/",$temp->verb->id);
+            // if($logArray[sizeof($logArray)-1]==="visited"){
+            //     $state[$i]['location'] = "outside" ;
+            //     $state[$i]['verb'] = $logArray[sizeof($logArray)-1];
+            // }else{
+            //     $state[$i]['location'] = "inside" ;
+            //     $state[$i]['verb'] = $logArray[sizeof($logArray)-1];
+            // }
+            // $state[$i]['user'] = $temp->actor ;
+            // $state[$i]['title'] = $temp->object->definition->name->en ;
             // $state[$i]['definition'] = $temp->object->definition ;
-            $state[$i]['timestamp'] = $temp->timestamp ;
+            // $state[$i]['timestamp'] = $temp->timestamp ;
         }
-        return($state) ;
+        dd($stmt_arr) ;
     }
 }
