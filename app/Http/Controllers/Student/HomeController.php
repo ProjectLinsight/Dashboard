082966c76a8fa3cf239ef767afd2a2102d288613
dilky97 <2017cs039@stu.ucsc.cmb.ac.lk>
2020-07-28 01:22:40 +0530

    Quiz Data and Student Feed completed

--- app/Http/Controllers/Student/HomeController.php
+++ app/Http/Controllers/Student/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Student;
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\Shared\sharedCourseXapi ;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller{
@@ -11,6 +14,22 @@ class HomeController extends Controller{
     }
 
     public function index(){
+        $my_statements = DB::table('stu_enrollments')->select('cid')->where('index',Auth::user()->index)->get()->toArray();
+        $enrolled_courses = Array();
+        $enrolled_courses_xapi = Array();
+        foreach($my_statements as $en){
+            $temp = $en->cid;
+            array_push($enrolled_courses,$temp);
+        }
+
+        $data = new sharedCourseXapi();
+        foreach($enrolled_courses as $ec){
+            $cur_course_stmts = $data->getData($ec);
+            array_push($enrolled_courses_xapi,$cur_course_stmts);
+        }
+
+        dd($enrolled_courses_xapi);
+
         return view('home');
     }
 }
