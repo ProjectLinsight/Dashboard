0f19e1f63e375a3bfe927e2fd5fa3285baf4a5bd
Raveesha Fernando <rdilshanf@gmail.com>
2020-08-06 16:19:36 +0530

    Code refactored

--- app/Http/Controllers/Student/PersonalCoursesController.php
+++ app/Http/Controllers/Student/PersonalCoursesController.php
@@ -19,14 +19,20 @@ class PersonalCoursesController extends Controller{
 
     public function index($course){
         $my_course = DB::table('stu_enrollments')->where('cid',$course)->where('index',Auth::user()->index)->get();
-
         $course_name  = Courses::where('cid',$course)->get();
         $reg_no = substr(Auth::user()->email,0,9);
 
+        //Start Date, Week and Course Duration
+        $today = date("Y-m-d");
+        $sDate = (DB::table('assign_lecturers')->where('cid',$course)->first())->startDate;
+        $sWeek = date("oW", strtotime($sDate));
+        $duration = 15 ;
+        // $duration = intval(date("oW",strtotime($today))) - intval(date("oW", strtotime($sDate))) + 1;
+
+        //Importing xApi Data
         $data = new sharedCourseXapi();
         $cur_course_stmts = $data->getData($course);
         $user_stmts = Array();
-
         foreach($cur_course_stmts as $st){
             if($st['user']->account->name == $reg_no){
                 array_push($user_stmts,$st);
@@ -34,27 +40,123 @@ class PersonalCoursesController extends Controller{
         }
 
         //Assignments Data
-        list($activity,$submittedAssignments,$gradedAssignments,$assignmentMarks) = $this->getAssignmentData($user_stmts);
-
-        //dd($activity,$submittedAssignments,$gradedAssignments,$assignmentMarks);
+        list($activity,$submittedAssignments,$gradedAssignments,$asMarks) = $this->getAssignmentData($user_stmts);
 
         //Quiz Data
         $quizArray = $this->getQuizData($user_stmts);
 
         //Viewed Resources
-
         $viewed_stmts = $this->getViewedData($user_stmts);
 
-        $today = date("Y-m-d");
-        $sDate = (DB::table('assign_lecturers')->where('cid',$course)->first())->startDate;
-        $sWeek = date("oW", strtotime($sDate));
-        // $duration = intval(date("oW",strtotime($today))) - intval(date("oW", strtotime($sDate))) + 1;
-        $duration = 15 ;
+        // Daily and weekly Figures
+        list($date_counts,$week_counts) = $this->getActivityOverTime($user_stmts,$today,$sDate,$duration);
 
+        // Assignment Positioning on Progress bar
+        list($subAssignments,$pendAssignments) = $this->getProgressbarPosition($course,$duration,$sDate,$submittedAssignments);
+
+        //Previous Results Overview
+        $prevResults = $this->getPrevResults($my_course[0]->cid);
+
+        return view('student.courses.personal',[
+            'crs'=> $course_name[0],
+            'gradedAssignments' => $gradedAssignments,
+            'submittedAssignments' => $submittedAssignments,
+            'weeklyAssignments' => $pendAssignments,
+            'subAssignments' => $subAssignments,
+            'quizzes'=>$quizArray,
+            'duration'=> $duration
+            ])
+            ->with('grade', json_encode($prevResults))
+            ->with('activity', json_encode($activity))
+            ->with('date_counts', json_encode($date_counts))
+            ->with('week_counts', json_encode($week_counts))
+            ->with('assignmentMarks', json_encode($asMarks))
+            ;
+    }
+
+    public function getAssignmentData($user_statements){
+        $activity[] = ['activity', 'Number'];
+        $submittedAssignments = array();
+        $gradedAssignments = array();
+        $assignmentMarks = array();
+        $activity = array(
+            "Viewed" => 0,
+            "Completed" => 0,
+            "Submitted" => 0,
+            "Graded" => 0,
+            "Received" => 0,
+            // "Visited" => 0,
+            // "Started" => 0,
+            // "Logged in" => 0,
+            // "Logged out" => 0,
+            // "Created" => 0,
+            // "Other" => 0,
+        );
+
+        foreach($user_statements as $us){
+            if("viewed"==$us["verb"]){ $activity["Viewed"]++; }
+            else if("completed"==$us["verb"]){
+                $activity["Completed"]++;
+                // array_push($submittedAssignments,$us);
+            }
+            else if("submitted"==$us["verb"]){
+                $activity["Submitted"]++;
+                array_push($submittedAssignments,$us);
+            }
+            else if("attained grade for"==$us["verb"]){
+                $activity["Graded"]++;
+                array_push($gradedAssignments,$us);
+            }
+            else if("received"==$us["verb"]){ $activity["Received"]++; }
+            // else if("visited"==$us["verb"]){ $activity["Visited"]++; }
+            // else if("started"==$us["verb"]){ $activity["Started"]++; }
+            // else if("logged into"==$us["verb"]){ $activity["Logged In"]++; }
+            // else if("logged out of"==$us["verb"]){ $activity["Logged Out"]++; }
+            // else if("enrolled to"==$us["verb"]){ $activity["Created"]++; }
+            // else{ $activity["Other"]++; }
+        }
+
+        foreach($gradedAssignments as $graded_stmt){
+            $temp = Array();
+            $temp['title'] = $graded_stmt['title'];
+            $temp['marksObtained'] = $graded_stmt['marks']/$graded_stmt['maxMarks']*100;
+            array_push($assignmentMarks,$temp);
+        }
+
+        $asMarks = Array();
+        foreach($assignmentMarks as $as){
+            $asMarks[$as["title"]]= $as["marksObtained"];
+        }
+
+        return array($activity,$submittedAssignments,$gradedAssignments,$asMarks);
+    }
+
+    public function getQuizData($user_statements){
+        $quizArray = Array();
+        foreach($user_statements as $quiz_stmt){
+            if($quiz_stmt['type']=="quiz"){
+                array_push($quizArray,$quiz_stmt);
+            }
+        }
+
+        return $quizArray;
+    }
+
+    public function getViewedData($user_statements){
+        $viewed_stmts = Array();
+        foreach($user_statements as $vs){
+            if($vs['verb']==="viewed" && $vs['object'] != "course"){
+                array_push($viewed_stmts,$vs);
+            }
+        }
+        return $viewed_stmts;
+    }
+
+    public function getActivityOverTime($user_stmts,$today,$sDate,$duration){
         //weekly figures
-        $weeklyFig = Array();
+        $week_counts = Array();
         for($i=1;$i<$duration;$i++){
-            $weeklyFig[$i]= 0 ;
+            $week_counts[$i]= 0 ;
         }
 
         //daily figures
@@ -73,12 +175,27 @@ class PersonalCoursesController extends Controller{
                 $loop = date("Y-m-d", $loop);
             }
             $weekNum = intval(date("oW",strtotime($us["date"]))) - intval(date("oW", strtotime($sDate))) + 1;
-            $weeklyFig[$weekNum]++;
+            $week_counts[$weekNum]++;
         }
+        return array($date_counts,$week_counts);
+    }
 
+    public function getPrevResults($courseCode){
+        $data = DB::table('results')->where('subjectCode',$courseCode)->select(
+                DB::raw('grade as grade'),
+                DB::raw('count(*) as number'))
+            ->groupBy('grade')->get();
 
+        $array[] = ['grade', 'Number'];
+        foreach($data as $key => $value){
+            $array[++$key] = [$value->grade, $value->number];
+        }
+        return $array;
+    }
+
+    public function getProgressbarPosition($course,$duration,$sDate,$submittedAssignments){
         $asmnts = DB::table('assignments')->where('cid',$course)->get();
-        $weeklyAssignments = Array();
+        $pendAssignments = Array();
         $subAssignments = Array();
         for($i=1;$i<$duration;$i++){
             ${"week"."$i"} = Array();
@@ -98,128 +215,9 @@ class PersonalCoursesController extends Controller{
             $temp = false ;
         }
         for($i=1;$i<$duration;$i++){
-            $weeklyAssignments[$i]= ${"week"."$i"} ;
+            $pendAssignments[$i]= ${"week"."$i"} ;
             $subAssignments[$i]= ${"weekSub"."$i"} ;
         }
-
-        // dd($weeklyAssignments);
-
-        //Results Overview
-        $data = DB::table('results')->where('subjectCode',$my_course[0]->cid)->select(
-                DB::raw('grade as grade'),
-                DB::raw('count(*) as number'))
-            ->groupBy('grade')->get();
-
-        $array[] = ['grade', 'Number'];
-        foreach($data as $key => $value){
-            $array[++$key] = [$value->grade, $value->number];
-        }
-        //End of Results Overview
-
-        $asMarks = Array();
-        foreach($assignmentMarks as $as){
-            $asMarks[$as["title"]]= $as["marksObtained"];
-        }
-        // dd($asMarks);
-
-
-        return view('student.courses.personal',[
-            'crs'=> $course_name[0],
-            'gradedAssignments' => $gradedAssignments,
-            'submittedAssignments' => $submittedAssignments,
-            'weeklyAssignments' => $weeklyAssignments,
-            'subAssignments' => $subAssignments,
-            'quizzes'=>$quizArray,
-            'duration'=> $duration
-            ])
-            ->with('grade', json_encode($array))
-            ->with('activity', json_encode($activity))
-            ->with('date_counts', json_encode($date_counts))
-            ->with('week_counts', json_encode($weeklyFig))
-            ->with('assignmentMarks', json_encode($asMarks))
-            ;
-    }
-
-    public function datediffInWeeks($date1, $date2)
-    {
-        $first = DateTime::createFromFormat('Y-m-d', $date1);
-        $second = DateTime::createFromFormat('Y-m-d', $date2);
-        return floor($first->diff($second)->days/7);
-    }
-
-    public function getAssignmentData($user_statements)
-    {
-        $activity[] = ['activity', 'Number'];
-        $submittedAssignments = array();
-        $gradedAssignments = array();
-        $assignmentMarks = array();
-        $activity = array(
-            // "Visited" => 0,
-            "Viewed" => 0,
-            // "Started" => 0,
-            "Completed" => 0,
-            "Submitted" => 0,
-            "Graded" => 0,
-            // "Logged in" => 0,
-            // "Logged out" => 0,
-            "Received" => 0,
-            // "Created" => 0,
-            // "Other" => 0,
-        );
-
-        foreach($user_statements as $us){
-            if("viewed"==$us["verb"]){ $activity["Viewed"]++; }
-            // else if("visited"==$us["verb"]){ $activity["Visited"]++; }
-            // else if("started"==$us["verb"]){ $activity["Started"]++; }
-            else if("completed"==$us["verb"]){
-                $activity["Completed"]++;
-                // array_push($submittedAssignments,$us);
-            }
-            else if("submitted"==$us["verb"]){
-                $activity["Submitted"]++;
-                array_push($submittedAssignments,$us);
-            }
-            else if("attained grade for"==$us["verb"]){
-                $activity["Graded"]++;
-                array_push($gradedAssignments,$us);
-            }
-            // else if("logged into"==$us["verb"]){ $activity["Logged In"]++; }
-            // else if("logged out of"==$us["verb"]){ $activity["Logged Out"]++; }
-            else if("received"==$us["verb"]){ $activity["Received"]++; }
-            // else if("enrolled to"==$us["verb"]){ $activity["Created"]++; }
-            // else{ $activity["Other"]++; }
-        }
-
-        foreach($gradedAssignments as $graded_stmt){
-            $temp = Array();
-            $temp['title'] = $graded_stmt['title'];
-            $temp['marksObtained'] = $graded_stmt['marks']/$graded_stmt['maxMarks']*100;
-            array_push($assignmentMarks,$temp);
-        }
-
-        return array($activity,$submittedAssignments,$gradedAssignments,$assignmentMarks);
-    }
-
-    public function getQuizData($user_statements){
-        $quizArray = Array();
-        foreach($user_statements as $quiz_stmt){
-            if($quiz_stmt['type']=="quiz"){
-                array_push($quizArray,$quiz_stmt);
-            }
-        }
-
-        return $quizArray;
-
-    }
-
-    public function getViewedData($user_statements)
-    {
-        $viewed_stmts = Array();
-        foreach($user_statements as $vs){
-            if($vs['verb']==="viewed" && $vs['object'] != "course"){
-                array_push($viewed_stmts,$vs);
-            }
-        }
-        return $viewed_stmts;
+        return array($subAssignments,$pendAssignments);
     }
 }
