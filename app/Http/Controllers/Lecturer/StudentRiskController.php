b8a73cf206513ec9fec169f6fcf2aa8043a0d565
gaganaw06 <43116731+gaganaw06@users.noreply.github.com>
2020-08-06 20:36:26 +0530

    edits

--- app/Http/Controllers/Lecturer/StudentRiskController.php
+++ app/Http/Controllers/Lecturer/StudentRiskController.php
@@ -9,6 +9,8 @@ use Illuminate\Support\Facades\DB;
 use App\Courses;
 use App\Http\Controllers\Shared\sharedXapi ;
 use App\Http\Controllers\Shared\sharedCourseXapi ;
+use App\Http\Controllers\Shared\sharedStudentCourseData;
+
 
 // use App\Stu_enrollment;
 
@@ -65,138 +67,137 @@ class StudentRiskController extends Controller{
             }
         }
 
+
+        $currentStuData = new sharedStudentCourseData();
+        
+        
+        
         //Assignments Data
 
-        $activity[] = ['activity', 'Number'];
-        $submittedAssignments = array();
-        $gradedAssignments = array();
-        $activity = array(
-            // "Visited" => 0,
-            "Viewed" => 0,
-            // "Started" => 0,
-            "Completed" => 0,
-            "Submitted" => 0,
-            "Graded" => 0,
-            // "Logged in" => 0,
-            // "Logged out" => 0,
-            "Received" => 0,
-            // "Created" => 0,
-            // "Other" => 0,
-        );
+        // $activity[] = ['activity', 'Number'];
+        // $submittedAssignments = array();
+        // $gradedAssignments = array();
+        // $activity = array(
+        //     "Viewed" => 0,
+        //     "Completed" => 0,
+        //     "Submitted" => 0,
+        //     "Graded" => 0,
+        //     "Received" => 0,
+        // );
 
-        foreach($user_stmts as $us){
-            if("viewed"==$us["verb"]){ $activity["Viewed"]++; }
-            // else if("visited"==$us["verb"]){ $activity["Visited"]++; }
-            // else if("started"==$us["verb"]){ $activity["Started"]++; }
-            else if("completed"==$us["verb"]){
-                $activity["Completed"]++;
-                // array_push($submittedAssignments,$us);
-            }
-            else if("submitted"==$us["verb"]){
-                $activity["Submitted"]++;
-                array_push($submittedAssignments,$us);
-            }
-            else if("attained grade for"==$us["verb"]){
-                $activity["Graded"]++;
-                array_push($gradedAssignments,$us);
-            }
-            // else if("logged into"==$us["verb"]){ $activity["Logged In"]++; }
-            // else if("logged out of"==$us["verb"]){ $activity["Logged Out"]++; }
-            else if("received"==$us["verb"]){ $activity["Received"]++; }
-            // else if("enrolled to"==$us["verb"]){ $activity["Created"]++; }
-            // else{ $activity["Other"]++; }
-        }
+        // foreach($user_stmts as $us){
+        //     if("viewed"==$us["verb"]){ $activity["Viewed"]++; }
+        //     else if("completed"==$us["verb"]){
+        //         $activity["Completed"]++;
+        //     }
+        //     else if("submitted"==$us["verb"]){
+        //         $activity["Submitted"]++;
+        //         array_push($submittedAssignments,$us);
+        //     }
+        //     else if("attained grade for"==$us["verb"]){
+        //         $activity["Graded"]++;
+        //         array_push($gradedAssignments,$us);
+        //     }
+        //     else if("received"==$us["verb"]){ $activity["Received"]++; }
+            
+        // }
 
-        // dd($gradedAssignments);
-
-        //Quiz Results
-        $quizArray = Array();
-        foreach($user_stmts as $quiz_stmt){
-            if($quiz_stmt['type']=="quiz"){
-                array_push($quizArray,$quiz_stmt);
-            }
-        }
+        // //Quiz Results
+        // $quizArray = Array();
+        // foreach($user_stmts as $quiz_stmt){
+        //     if($quiz_stmt['type']=="quiz"){
+        //         array_push($quizArray,$quiz_stmt);
+        //     }
+        // }
 
         //dd($quizArray);
 
         $today = date("Y-m-d");
         $sDate = (DB::table('assign_lecturers')->where('cid',$course)->first())->startDate;
         $sWeek = date("oW", strtotime($sDate));
-        // $duration = intval(date("oW",strtotime($today))) - intval(date("oW", strtotime($sDate))) + 1;
         $duration = 15 ;
 
         //weekly figures
-        $weeklyFig = Array();
-        for($i=1;$i<$duration;$i++){
-            $weeklyFig[$i]= 0 ;
-        }
+        // $weeklyFig = Array();
+        // for($i=1;$i<$duration;$i++){
+        //     $weeklyFig[$i]= 0 ;
+        // }
 
-        //daily figures
-        $loop = $sDate;
-        while($loop!=$today){
-            $date_counts[$loop] = 0;
-            $loop = strtotime("+1 day", strtotime($loop));
-            $loop = date("Y-m-d", $loop);
-        }
+        // //daily figures
+        // $loop = $sDate;
+        // while($loop!=$today){
+        //     $date_counts[$loop] = 0;
+        //     $loop = strtotime("+1 day", strtotime($loop));
+        //     $loop = date("Y-m-d", $loop);
+        // }
 
-        foreach($user_stmts as $us){
-            $loop = $sDate;
-            while($loop!=$today){
-                if($loop==$us["date"]){ $date_counts[$loop]++; }
-                $loop = strtotime("+1 day", strtotime($loop));
-                $loop = date("Y-m-d", $loop);
-            }
-            $weekNum = intval(date("oW",strtotime($us["date"]))) - intval(date("oW", strtotime($sDate))) + 1;
-            $weeklyFig[$weekNum]++;
-        }
+        // foreach($user_stmts as $us){
+        //     $loop = $sDate;
+        //     while($loop!=$today){
+        //         if($loop==$us["date"]){ $date_counts[$loop]++; }
+        //         $loop = strtotime("+1 day", strtotime($loop));
+        //         $loop = date("Y-m-d", $loop);
+        //     }
+        //     $weekNum = intval(date("oW",strtotime($us["date"]))) - intval(date("oW", strtotime($sDate))) + 1;
+        //     $weeklyFig[$weekNum]++;
+        // }
 
 
-        $asmnts = DB::table('assignments')->where('cid',$course)->get();
-        $weeklyAssignments = Array();
-        $subAssignments = Array();
-        for($i=1;$i<$duration;$i++){
-            ${"week"."$i"} = Array();
-            ${"weekSub"."$i"} = Array();
-        }
-        $temp = false ;
-        foreach($asmnts as $as){
-            foreach($submittedAssignments as $sa){
-                if($sa["title"] == $as->title){
-                    $temp = true ;
-                    continue ;
-                }
-            }
-            $wNo = intval(date("oW",strtotime($as->dueDate))) - intval(date("oW", strtotime($sDate))) + 1;
-            if($temp){array_push( ${"weekSub"."$wNo"} , $as);}
-            else{array_push( ${"week"."$wNo"} , $as);}
-            $temp = false ;
-        }
-        for($i=1;$i<$duration;$i++){
-            $weeklyAssignments[$i]= ${"week"."$i"} ;
-            $subAssignments[$i]= ${"weekSub"."$i"} ;
-        }
+        // $asmnts = DB::table('assignments')->where('cid',$course)->get();
+        // $weeklyAssignments = Array();
+        // $subAssignments = Array();
+        // for($i=1;$i<$duration;$i++){
+        //     ${"week"."$i"} = Array();
+        //     ${"weekSub"."$i"} = Array();
+        // }
+        // $temp = false ;
+        // foreach($asmnts as $as){
+        //     foreach($submittedAssignments as $sa){
+        //         if($sa["title"] == $as->title){
+        //             $temp = true ;
+        //             continue ;
+        //         }
+        //     }
+        //     $wNo = intval(date("oW",strtotime($as->dueDate))) - intval(date("oW", strtotime($sDate))) + 1;
+        //     if($temp){array_push( ${"weekSub"."$wNo"} , $as);}
+        //     else{array_push( ${"week"."$wNo"} , $as);}
+        //     $temp = false ;
+        // }
+        // for($i=1;$i<$duration;$i++){
+        //     $weeklyAssignments[$i]= ${"week"."$i"} ;
+        //     $subAssignments[$i]= ${"weekSub"."$i"} ;
+        // }
 
-        // dd($weeklyAssignments);
 
-        //Results Overview
-        $data = DB::table('results')->where('subjectCode',$course)->select(
-                DB::raw('grade as grade'),
-                DB::raw('count(*) as number'))
-            ->groupBy('grade')->get();
+        // //Results Overview
+        // $data = DB::table('results')->where('subjectCode',$course)->select(
+        //         DB::raw('grade as grade'),
+        //         DB::raw('count(*) as number'))
+        //     ->groupBy('grade')->get();
 
-        $array[] = ['grade', 'Number'];
-        foreach($data as $key => $value){
-            $array[++$key] = [$value->grade, $value->number];
-        }
-        //End of Results Overview
+        // $array[] = ['grade', 'Number'];
+        // foreach($data as $key => $value){
+        //     $array[++$key] = [$value->grade, $value->number];
+        // }
+        // //End of Results Overview
 
-        //Viewed Resources
-        $viewed_stmts = Array();
-        foreach($user_stmts as $vs){
-            if($vs['verb']==="viewed" && $vs['object'] != "course"){
-                array_push($viewed_stmts,$vs);
-            }
-        }
+        // //Viewed Resources
+        // $viewed_stmts = Array();
+        // foreach($user_stmts as $vs){
+        //     if($vs['verb']==="viewed" && $vs['object'] != "course"){
+        //         array_push($viewed_stmts,$vs);
+        //     }
+        // }
+
+        //Assignments Data
+       list($activity,$submittedAssignments,$gradedAssignments,$asMarks) = $currentStuData->getAssignmentData($user_stmts);
+
+       //Quiz Data
+       $quizArray = $currentStuData->getQuizData($user_stmts);
+
+       // Daily and weekly Figures
+       list($date_counts,$week_counts) = $currentStuData->getActivityOverTime($user_stmts,$today,$sDate,$duration);
+       
 
         $note=$this->noteCount($course,$student);
         $link=$this->getLinks($student);
@@ -208,17 +209,17 @@ class StudentRiskController extends Controller{
             'user'=>$user,
             'gradedAssignments' => $gradedAssignments,
             'submittedAssignments' => $submittedAssignments,
-            'weeklyAssignments' => $weeklyAssignments,
-            'subAssignments' => $subAssignments,
+            // 'weeklyAssignments' => $weeklyAssignments,
+            // 'subAssignments' => $subAssignments,
             'quizzes'=>$quizArray,
             'duration'=> $duration
             ])
             ->with('risks', $risk)
             ->with('stuDetails', $reg_no)
-            ->with('grade', json_encode($array))
+            // ->with('grade', json_encode($array))
             ->with('activity', json_encode($activity))
             ->with('date_counts', json_encode($date_counts))
-            ->with('week_counts', json_encode($weeklyFig))
+            ->with('week_counts', json_encode($week_counts))
             ->with('assGraph', json_encode($graph_arr))
             ->with('notes', $note)
             ->with('risks', $risk)
