b6e0494e73435713634da5c5c7fd6ee63d7fdcef
gaganaw06 <43116731+gaganaw06@users.noreply.github.com>
2020-08-19 18:04:43 +0530

    edits

--- resources/views/lecturer/overview.blade.php
+++ resources/views/lecturer/overview.blade.php
@@ -112,7 +112,8 @@
                         </div>
                     </div>
                 </div>
-                <div class="card shadow">
+                <div class="pb-4">
+                    <div class="card shadow">
                                 <div class="card-header bg-primary">
                                     <h4 class="my-0 text-white text-center"> Lecture Note Completion </h4>
                                 </div>
@@ -132,7 +133,7 @@
                                                 }
                                                 ?>
                                                     <div class="py-3">
-                                                        <div class="milestone2" style="left:{{$padding}};cursor: pointer;" >
+                                                        <div class="milestone4" style="left:{{$padding}};cursor: pointer;" >
                                                             <h5 class="text-{{$color}}" data-toggle="modal" data-target="#assignmentModel">
                                                                 <i class="fa fa-address-book-o" aria-hidden="true"  data-toggle="tooltip" data-placement="top"  title="Assignment/s"></i>
                                                             </h5>
@@ -141,7 +142,8 @@
                                                 
                                                     <div class="py-3">
                                                         <div class="milestone3 d-flex justify-content-center" style="left:{{$padding}};cursor: pointer;" data-toggle="tooltip" data-placement="top"  title="{{$key}}" >
-                                                           <p class="text-primary"> <strong> {{$key}} </strong> </p>
+                                                           <p class="text-primary" style="margin-top:10px;margin-left:-5px"> <strong> {{$key}} </strong> </p>
+                                                           <p class="text-muted" style="margin-top:22px;margin-left:-50px" > <small>Viewed {{$val['count']}} of {{$val['enrolled']}} </small> </p>
                                                         </div>
                                                     </div>
                                                 <div class="milestone bg-{{$color}}" style="left:{{$padding}};cursor: pointer;" data-toggle="tooltip" data-placement="top"  title="{{$key}}" > </div>
